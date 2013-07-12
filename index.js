9cc42a81a9e935af5b718b9c6a44109c91bd142f
David Roncancio <david.roncancio@gmail.com>
2013-07-11 20:28:26 -0500

    Allow CORS

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ var server = restify.createServer();
 server.use(restify.queryParser({ mapParams: false }));
 server.use(restify.requestLogger());
 
+server.use(
+  function crossOrigin(req,res,next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if ('OPTIONS' === req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+  }
+);
+
 var restroom = function(database){
     var db = mongojs(database);
 
